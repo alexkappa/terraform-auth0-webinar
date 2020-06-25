c1c8a12725a0d52c121c510c42a47dddcafb4278
Alex Kalyvitis <alexkappa@github.com>
2020-06-25 20:24:43 +0200

    add roles and users

--- main.tf
+++ main.tf
@@ -2,28 +2,57 @@ provider "auth0" {
   version = "~> 0.11"
 }
 
-resource "auth0_resource_server" "my_server" {
+resource "auth0_resource_server" "blog" {
   name       = "Blog Post API"
   identifier = "https://api.blog.com"
 
   scopes {
-    value       = "create:post"
-    description = "Create posts"
+    value       = "write:post"
+    description = "Write posts"
   }
 
   scopes {
-    value       = "view:post"
-    description = "View posts"
+    value       = "review:post"
+    description = "Review posts"
   }
 }
 
-resource "auth0_client" "my_client" {
-  name = "Blog Post Bot 🚀"
-  app_type = "non_interactive"
+resource "auth0_role" "reviewer" {
+  name        = "Reviewer"
+  description = "Blog post reviewer"
+
+  permissions {
+    name                       = "review:post"
+    resource_server_identifier = auth0_resource_server.blog.identifier
+  }
 }
 
-resource "auth0_client_grant" "my_client_grant" {
-  client_id = auth0_client.my_client.client_id
-  audience = auth0_resource_server.my_server.identifier
-  scope = ["create:post"]
+resource "auth0_role" "writer" {
+  name        = "Writer"
+  description = "Blog post writer"
+
+  permissions {
+    name                       = "write:post"
+    resource_server_identifier = auth0_resource_server.blog.identifier
+  }
+}
+
+resource "auth0_user" "jane_doe" {
+  connection_name = "Username-Password-Authentication"
+
+  email = "jane.doe@blog.com"
+  username = "jane"
+  password = "I writez teh blogz!"
+
+  roles = [auth0_role.writer.id]
+}
+
+resource "auth0_user" "john_doe" {
+  connection_name = "Username-Password-Authentication"
+
+  email = "john.doe@blog.com"
+  username = "john"
+  password = "I review dem blogz!"
+
+  roles = [auth0_role.reviewer.id]
 }
